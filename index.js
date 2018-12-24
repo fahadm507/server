28de93d16c4a60c76f8d31fac68f44e3fd52cc85
Fahad Mukunzi <fahadm507@gmail.com>
2018-12-24 11:42:41 -0500

    Implements google-auth with passport.js

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
 
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token ", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google',{
+    scope: ['profile', 'email']
+  })
+);
+
+//route to handle callback from google
+app.get('/auth/google/callback',
+    passport.authenticate('google'));
 
 //use port that was defined by Heroku or 5000 if it's undefined
 const PORT = process.env.PORT || 5000;
