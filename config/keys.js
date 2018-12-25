7b0f6cd4b2531a813a22afa57e9d92a866809908
Fahad Mukunzi <fahadm507@gmail.com>
2018-12-24 20:46:32 -0500

    Fix typo in keys.js

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@
   file will not be committed to Github.
   keys.js - figure out what set of credentials to return
 */
-if (process.env.NODE_DEV === 'production'){
+if (process.env.NODE_ENV === 'production'){
   //we are in production - return the prod set of keys
   module.exports = require('./prod');
 }else {
