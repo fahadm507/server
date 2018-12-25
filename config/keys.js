c55bb7f29d6c72f5ccb25e85d60391f1614bf0d9
Fahad Mukunzi <fahadm507@gmail.com>
2018-12-24 20:39:42 -0500

    comment out what was mean't to be comments

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
   commit this file so that the app always knows what keys to use.
   whether in development or production. Note that development(dev.js)
   file will not be committed to Github.
+  keys.js - figure out what set of credentials to return
 */
-keys.js - figure out what set of credentials to return
 if (process.env.NODE_DEV === 'production'){
   //we are in production - return the prod set of keys
   module.exports = require('./prod');
