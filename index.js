1ff1a74541926f7702b413f6d1c7ef12b86aa123
Fahad Mukunzi <fahadm507@gmail.com>
2018-12-27 08:30:35 -0500

    Adds redirect routes and cleans up server-side code

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
-const keys = require('./config/keys');
-require('./models/User');
+const keys = require("./config/keys");
+require("./models/User");
 //we require User model before we require passport service, because that's where the user is being used.
-require('./services/passport');
+require("./services/passport");
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(cookieSession({
+app.use(
+  cookieSession({
     //expire in 30days, in milliseconds
     mexAge: 30 * 24 * 60 * 60 * 1000,
     //encrypt the cookie
@@ -22,8 +23,9 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app)
+require("./routes/authRoutes")(app);
 
 //use port that was defined by Heroku or 5000 if it's undefined
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
+console.log("listening on port == ", PORT);
+app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,24 +1,27 @@
-
-const passport = require('passport');
-module.exports = (app) => {
+const passport = require("passport");
+module.exports = app => {
   app.get(
-    '/auth/google',
-     passport.authenticate('google',{
-       scope: ['profile', 'email']
-     })
-   );
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
 
-   app.get('/api/logout', (req, res) => {
-     req.logout();
-     res.send(req.user);
-   });
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
-   //route to handle callback from google
-   app.get('/auth/google/callback',
-      passport.authenticate('google'));
+  //route to handle callback from google
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/goals");
+    }
+  );
 
-    app.get('/api/current_user', (req, res) => {
-        res.send(req.user)
-    });
-
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
