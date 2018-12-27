efb530256792cfa1e48db6206819baa2e8908803
Fahad Mukunzi <fahadm507@gmail.com>
2018-12-27 09:27:47 -0500

    handles routing in production env.

--- index.js
+++ index.js
@@ -25,7 +25,17 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up production assets
+  //like our main.js or main.css file!
+  app.use(express.static("client/build"));
+  //Express will serve up the index.hml file
+  //if it doesn't recognize the route.
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //use port that was defined by Heroku or 5000 if it's undefined
 const PORT = process.env.PORT || 5000;
-console.log("listening on port == ", PORT);
 app.listen(PORT);
