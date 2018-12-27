446cb8043084e6babe2ccff4c143b9bed8091daa
Fahad Mukunzi <fahadm507@gmail.com>
2018-12-27 10:01:12 -0500

    Add a script to let Heroku to handle the production build

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
+  //if the route is not defined by Express,
   //Express will serve up production assets
   //like our main.js or main.css file!
   app.use(express.static("client/build"));
+
+  //if the above doesn't resolve, it means that we
+  //don't understand the route request.
   //Express will serve up the index.hml file
-  //if it doesn't recognize the route.
   const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
